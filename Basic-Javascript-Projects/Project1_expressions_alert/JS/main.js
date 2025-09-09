8275134a57d7fb1b5638e67e8c06915f43713a47
Erica Millan <ericamillan1234@gmail.com>
2025-09-09 11:52:08 -0500

    Added some basic javascript

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,26 @@
+/*window.alert("Hello, World!");*/
+// document.write("hello, world!");
+// var A = "This is a string";
+// document.write("This is a string with \" quotes");
+// document.write("This is a string that is concatenated" + " with a new string.");
+// var B ="Concatented" + " String"
+// document.write(B)
+// var Family = "The Millers", Dad = "Michael", Mom = "Jennifer", Daughter = "Emily", Brother = "John";
+// document.write("In" + Family + "family, the father is " + Dad + " the mother is " + Mom);
+// document.write(Dad);
+// document.write("In the " + Dad + "and" + Mom + family);
+// document.write(3+3);
+
+//Javascript Assignment:
+
+//Window Alert for Hello world
+window.alert("Hello, world!");
+//Document write string
+document.write("Example using document.write");
+//Creating Variables
+var Sent1="This is the beginning of the string";
+var Sent2=" and this is the end of the string";
+//Concat variables
+var Concat = Sent1 + Sent2;
+//Output concatenated variables
+document.write("The concatenated string is: " + Concat);
